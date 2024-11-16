dc2fe2dfe645f0aa36c444ae946e5716ad6c3380
fh95-tech <fakih.hussein995@gmail.com>
2024-11-16 21:18:13 +0200

    <linkedlist> create class LinkedList and initiated it

--- linkedList.php
+++ linkedList.php
@@ -3,12 +3,19 @@ class Node{
     public $value;
     public $next;
 
-    public function __construct($value)
-    {   
+    public function __construct($value){   
         $this->value=$value;
         $this->next=null;
     }
 }
 
+class LinkedList{
+    public $head;
+
+    public function __construct(){
+        $this->head=null;
+    }
+    
+}
 
 ?>
