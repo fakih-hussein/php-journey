ac01e98fa21c9e9c7097db1c2ecd6d02988d483f
fh95-tech <fakih.hussein995@gmail.com>
2024-11-16 20:22:26 +0200

    <userClass> added email validation function

--- userClass.php
+++ userClass.php
@@ -1,23 +1,33 @@
 <?php
-class User{
+class User
+{
     private $email;
     private $password;
-    
-    public function __construct($email,$password)
+
+    public function __construct($email, $password)
     {
-        $this->email=$email;
-        $this->password=$password;
+        $this->email = $email;
+        $this->password = $password;
     }
 
-    public static function checkPassword($password){
-        $hasUpperCase=preg_match("/[A-Z]/",$password);
-        $hasLowerCase=preg_match("/[a-z]/",$password);
-        $hasSpecial=preg_match("/[\W]/",$password);
-        $hasMinLen=strlen($password)>=12;
+    public static function validateEmail($email)
+    {
+        $pattern = '/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+        $valid = preg_match($pattern, $email);
+        if ($valid) {
+            return $valid;
+        } else {
+            return "Invalid email.";
+        }
+    }
+
+    public static function checkPassword($password)
+    {
+        $hasUpperCase = preg_match("/[A-Z]/", $password);
+        $hasLowerCase = preg_match("/[a-z]/", $password);
+        $hasSpecial = preg_match("/[\W]/", $password);
+        $hasMinLen = strlen($password) >= 12;
 
         return $hasLowerCase && $hasUpperCase && $hasSpecial && $hasMinLen;
     }
 }
-
-
-?>
