1dfcce097aeca3266e8b6bc27e786e689ecf0de6
fh95-tech <fakih.hussein995@gmail.com>
2024-11-16 19:00:58 +0200

    <merge sort> added merge function

--- mergeSort.php
+++ mergeSort.php
@@ -6,6 +6,19 @@ function mergeSort($array) {
     $mid = floor(count($array) / 2);
     $left = mergeSort(array_slice($array, 0, $mid));
     $right = mergeSort(array_slice($array, $mid));
+    return merge($left, $right);
+}
+
+function merge($left, $right) {
+    $result = [];
+    while (count($left) > 0 && count($right) > 0) {
+        if ($left[0] < $right[0]) {
+            $result[] = array_shift($left);
+        } else {
+            $result[] = array_shift($right);
+        }
+    }
+    return array_merge($result, $left, $right);
 }
 
 
